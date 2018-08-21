4eb9da86434a95973bbf732e2f3a82d585c5309b
bai <baiyunhan.cs@gmail.com>
2018-08-21 20:08:20 +0800

    add lstm attention model.

---
+++ practice/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+#@Filename : __init__.py
+#@Date : 2018-08-21-17-51
+#@AUTHOR : bai
---
+++ practice/text_classification/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+#@Filename : __init__.py
+#@Date : 2018-08-21-17-51
+#@AUTHOR : bai
+
